f604dfa61d96c22b7f33f5a8bf9b8f19f86d2bca
hrnoh <hrnoh24@gmail.com>
2017-06-21 16:06:32 +0900

    add streaming

--- WebSocket/Client/client.py
+++ WebSocket/Client/client.py
@@ -8,42 +8,38 @@ import uuid
 import datetime
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(4, GPIO.OUT)
-
-# Rule 설정
-rule = {1: {'card':1, 'face':0, 'finger':0},
-        2: {'card':1, 'face':1, 'finger':0},
-        3: {'card':1, 'face':1, 'finger':1}}
-
 class Client:
     def __init__(self):
         self.mac = get_mac()
-        self.exitFlag = 0
-        #self.lock = threading.Event()
+        self.connectionCount = 0
+        self.evt = threading.Event()
 
         # 5회 연결 시도
         for i in range(5):
             try:
-                self.ws = websocket.create_connection("ws://172.30.1.10:8080/doorlock/doorlock-ws")
+                self.ws = websocket.create_connection("ws://172.30.1.17:8080/doorlock-ws")
                 break
             except ConnectionRefusedError as e:
-                print(e.args)
+                self.connectionCount += 1
+                print("연결 실패, 재시도 횟수 : %i" % self.connectionCount)
                 time.sleep(2)
 
+        if self.connectionCount >= 5:
+            print("서버에 연결할 수 없습니다...")
+            sys.exit(-1)
+
         self.recvThread = threading.Thread(target=self.recv)
-        self.recvThread.daemon = True
         self.recvThread.start()
         self.service()
 
     def recv(self):
-        try:
-            while not self.exitFlag:
+        while True:
+            try:
                 msg = self.ws.recv()
                 body = json.loads(msg)
 
                 # 기능별 수행
-                if body['type'] == 100:     # doorlock open
+                if body['type'] == 100:  # doorlock open
                     data = json.loads(body['data'])
 
                     self.dno = data['dno']
@@ -58,12 +54,13 @@ class Client:
                 # info response : 사원 번호, 사원 이름, level, status
                 elif body['type'] == 300:
                     employee = json.loads(body['data'])
-                    #print(employee)
 
                     result = self.certification(employee['level'])
 
                     if result == True:
                         print("인증 성공")
+                        openThread = threading.Thread(target=self.door_open)
+                        openThread.start()
                         self.access_result(employee['eno'], self.dno, "success")
                     else:
                         print("인증 실패")
@@ -72,15 +69,17 @@ class Client:
                 # remoteOpen : 원격 개방
                 elif body['type'] == 400:
                     # 도어락 여는 코드
-                    door_open()
+                    openThread = threading.Thread(target=self.door_open)
+                    openThread.start()
+                    print("원격 개방 실행")
 
                 # Error 메시지
                 elif body['type'] == 700:
                     data = body['data']
                     print(data)
-                    
-        except Exception:
-            shutdown()
+
+            except Exception:
+                self.shutdown()
 
 
 
@@ -88,15 +87,16 @@ class Client:
     def service(self):
         self.init()
 
-        try:
-            while not self.exitFlag:
+        while True:
+            try:
                 # 카드키 접촉 대기(test를 위해 사용자 입력 받음)
                 time.sleep(3)
                 eno = input("사원 번호 입력 : ")
                 self.cert_request(int(eno))
-                
-        except Exception:
-            shutdown()
+
+            except Exception:
+                print(Exception.args[0])
+                self.shutdown()
 
     def sendLog(self, ** kwargs):
         message = json.dumps(kwargs)
@@ -104,14 +104,14 @@ class Client:
 
     # 초기화
     def init(self):
-        type = 'dinit'
+        type = 100
         msg = {'type':type, 'data':self.mac}
         #print(json.dumps(msg))
         self.ws.send(json.dumps(msg))
 
     # 인증 요청
     def cert_request(self, eno):
-        msg_dict = {'type':'info_request', 'data': eno}
+        msg_dict = {'type':300, 'data': eno}
         msg_json = json.dumps(msg_dict)
         self.ws.send(msg_json)
 
@@ -125,9 +125,6 @@ class Client:
 
         if proc['face']:
             result_set['face'] = self.face(level)
-
-        if proc['finger']:
-            result_set['finger'] = self.finger(level)
 
         proc_list = list(proc.values())
         result_set_list = list(result_set.values())
@@ -151,15 +148,16 @@ class Client:
         #print('face 인증')
         return 1
 
-    # 지문 인증
-    def finger(self, level):
-        #print('finger 인증')
-        return 1
-
-    # Shutdown
-    def shutdown(self):
-        GPIO.cleanup()
-        sys.exit()
+    # Doorlock Open
+    def door_open(self):
+        if self.evt.isSet() == False:
+            self.evt.set()
+            GPIO.output(4, True)
+            time.sleep(0.5)
+            GPIO.output(4, False)
+            self.evt.clear()
+        else:
+            sys.exit()
 
     # 출입시도 결과 보내기
     def access_result(self, eno, dno, result):
@@ -167,7 +165,7 @@ class Client:
         log_json = json.dumps(log)
         message = {'type':350, 'data':log_json}
         self.ws.send(json.dumps(message))
-        
+
 
 # Mac Address 받아오기
 def get_mac():
@@ -175,14 +173,19 @@ def get_mac():
     mac = ':'.join(mac_num[i : i + 2] for i in range(0, 11, 2))
     return mac
 
-# Doorlock Open
-def door_open():
-    for i in range(2):
-        GPIO.output(4, True)
-        time.sleep(0.5)
-        GPIO.output(4, False)
-        time.sleep(10)
-
 
 if __name__ == "__main__":
-    clnt = Client()
+    try:
+        # GPIO 활성화
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(4, GPIO.OUT)
+
+        # Rule 설정
+        rule = {1: {'card': 1, 'face': 0},
+                2: {'card': 1, 'face': 1},
+                3: {'card': 1, 'face': 1}}
+
+        clnt = Client()
+    finally:
+        GPIO.cleanup()
+        pass
